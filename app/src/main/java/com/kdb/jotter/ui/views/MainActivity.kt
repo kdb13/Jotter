670bfe76ee30714e101012c8c3fc8789ea67455d
Kahan D. Bhalani <kanbhalani@gmail.com>
2022-02-23 13:59:40 +0530

    Use MaterialToolbar as the action bar
    Using a dedicated Toolbar widget to represent the action bar. Made some
    necessary changes related to UI Layout and Navigation Component to use
    this Toolbar.
    
    Also, removed statusBarColor from the Night theme (uses the default
    black color now).

--- app/src/main/java/com/kdb/jotter/ui/views/MainActivity.kt
+++ app/src/main/java/com/kdb/jotter/ui/views/MainActivity.kt
@@ -2,18 +2,26 @@ package com.kdb.jotter.ui.views
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.kdb.jotter.R
+import com.kdb.jotter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.topAppBar)
 
         setupNavigation()
     }
@@ -24,12 +32,9 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        // Set up the ActionBar with Navigation component
-        setupActionBarWithNavController(navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        // Set up the Toolbar with Navigation Component
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.topAppBar.setupWithNavController(navController, appBarConfiguration)
     }
 
 }
