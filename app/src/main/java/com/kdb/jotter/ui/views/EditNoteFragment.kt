0362802f77c15f086607e084a942732d3972d113
Kahan D. Bhalani <kanbhalani@gmail.com>
2022-12-24 11:07:40 +0530

    Use new menu APIs
    The fragment now uses the new MenuHost and MenuProvider APIs for
    displaying the menu for EditNote screen. The menu will only be added
    once the note details are loaded successfully.

--- app/src/main/java/com/kdb/jotter/ui/views/EditNoteFragment.kt
+++ app/src/main/java/com/kdb/jotter/ui/views/EditNoteFragment.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -18,25 +21,21 @@ import com.kdb.jotter.ui.showSoftKeyboard
 import com.kdb.jotter.ui.viewmodels.EditNoteViewModel
 import com.kdb.jotter.ui.viewmodels.EditNoteViewModelFactory
 
-class EditNoteFragment : Fragment() {
+class EditNoteFragment : Fragment(), MenuProvider {
 
     private var _binding: FragmentEditNoteBinding? = null
     private val binding get() = _binding!!
 
     private val args: EditNoteFragmentArgs by navArgs()
 
+    // To prevent duplicate menu additions to the MenuHost
+    private var isMenuAdded = false
+
     private val viewModel: EditNoteViewModel by viewModels {
         EditNoteViewModelFactory(
             (activity?.application as JotterApplication).repository,
             args.noteID
         )
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // Enable options menu
-        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -114,8 +113,11 @@ class EditNoteFragment : Fragment() {
         viewModel.uiState.observe(viewLifecycleOwner) { state ->
             binding.state = state
 
-            if (!state.isMenuHidden) {
-                activity?.invalidateOptionsMenu()
+            if (!state.isLoading && !isMenuAdded) {
+                val menuHost = requireActivity() as MenuHost
+                menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+                isMenuAdded = true
             }
 
             Log.d(TAG, "observeState: $state")
@@ -126,27 +128,12 @@ class EditNoteFragment : Fragment() {
         OPTIONS MENU
         ------------
      */
-    override fun onPrepareOptionsMenu(menu: Menu) {
-        super.onPrepareOptionsMenu(menu)
-
-        Log.d(TAG, "onPrepareOptionsMenu: ")
-
-        viewModel.uiState.value?.let { state ->
-            menu.findItem(R.id.action_share).isVisible = !state.isMenuHidden
-            menu.findItem(R.id.action_delete).isVisible = !state.isMenuHidden
-        }
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_edit_note, menu)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-
-        Log.d(TAG, "onCreateOptionsMenu: ")
-
-        inflater.inflate(R.menu.menu_edit_note, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
             R.id.action_delete -> {
                 if (viewModel.isNoteEmpty()) {
                     // Directly delete, without confirmation
@@ -162,7 +149,7 @@ class EditNoteFragment : Fragment() {
                 true
             }
 
-            else -> super.onOptionsItemSelected(item)
+            else -> false
         }
     }
 }
